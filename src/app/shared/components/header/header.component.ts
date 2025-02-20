67e2c8b11ced915f689dda58b8bb9796aeea11ae
leonaldopasaribu <leonaldopasaribu13@yahoo.com>
2025-02-20 21:00:14 +0700

    feat: add footer component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -4,15 +4,4 @@ import { Component } from '@angular/core';
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {
-  readonly navLinks = [
-    {
-      label: 'CATEGORIES',
-      route: '/categories',
-      icon: 'assets/icons/menu.svg',
-    },
-    { label: 'MOVIES', route: '/movies' },
-    { label: 'TV SHOWS', route: '/tv-shows' },
-    { label: 'LOGIN', route: '/login' },
-  ];
-}
+export class HeaderComponent {}
