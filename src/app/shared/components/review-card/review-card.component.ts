ff7e60c035e5aad11bba5f99cf6c26e2309aba22
leonaldopasaribu <leonaldopasaribu13@yahoo.com>
2025-02-22 13:02:00 +0700

    style: adjust review card skeleton loading

--- src/app/shared/components/review-card/review-card.component.ts
+++ src/app/shared/components/review-card/review-card.component.ts
@@ -1,13 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-review-card',
   templateUrl: './review-card.component.html',
 })
 export class ReviewCardComponent {
-  @Input() name: string = '';
-  @Input() date: string = '';
-  @Input() highlightMessage: string = '';
-  @Input() message: string = '';
-  @Input() rating: string = '';
+  readonly date = input<string>('');
+  readonly highlightMessage = input<string>('');
+  readonly isLoading = input<boolean>(false);
+  readonly message = input<string>('');
+  readonly name = input<string>('');
+  readonly rating = input<string>('');
 }
